6b87fe87ba1ba169765e3d27d83f2af922d70343
bdbrink <brendan.brink@outlook.com>
2023-08-08 17:06:26 -0500

    adding stock price, sma, and ema funcs

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ import os
 
 # use export command to generate
 openai.api_key = os.environ['OPEN_API_KEY']
+
+def stock_price_price(ticker):
+    return str(yf.Ticker(ticker).history(period="1y").iloc[-1].Close)
+
+def calculate_SMA(ticker, window):
+    data = yf.Ticker(ticker).history(period="1y").Close
+    return str(data.rolling(window=window).mean().iloc[-1])
+
+def calculate_EMA(ticker, window):
+    data = yf.Ticker(ticker).history(period="1y").Close
+    return str(data.ewm(span=window).mean().iloc[-1])
