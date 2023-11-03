c5de78fb87ebd1de1d05845ced144d366755d31a
bdbrink <brendan.brink@outlook.com>
2023-11-03 16:19:27 -0500

    add try catch improve readability

--- main.py
+++ main.py
@@ -1,67 +1,84 @@
 import json
 import openai
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 import yfinance as yf
-import os
 
-# Use export command to generate
-openai.api_key = os.environ["OPEN_API_KEY"]
+# Check if the required environment variable is set
+openai.api_key = os.getenv("OPEN_API_KEY")
+if openai.api_key is None:
+    st.error("OPEN_API_KEY environment variable is not set.")
+    st.stop()
 
 # Function to get the latest stock price for a given ticker
-
-
 def get_latest_stock_price(ticker):
-    return str(yf.Ticker(ticker).history(period="1y").iloc[-1].Close)
+    try:
+        data = yf.Ticker(ticker).history(period="1y")
+        return str(data.iloc[-1].Close)
+    except Exception as e:
+        return str(e)
 
 # Function to calculate the Simple Moving Average (SMA) for a given ticker and window size
-
-
 def calculate_SMA(ticker, window):
-    data = yf.Ticker(ticker).history(period="1y").Close
-    return str(data.rolling(window=window).mean().iloc[-1])
+    try:
+        data = yf.Ticker(ticker).history(period="1y").Close
+        return str(data.rolling(window=window).mean().iloc[-1])
+    except Exception as e:
+        return str(e)
 
 # Function to calculate the Exponential Moving Average (EMA) for a given ticker and window size
-
-
 def calculate_EMA(ticker, window):
-    data = yf.Ticker(ticker).history(period="1y").Close
-    return str(data.ewm(span=window).mean().iloc[-1])
+    try:
+        data = yf.Ticker(ticker).history(period="1y").Close
+        return str(data.ewm(span=window).mean().iloc[-1])
+    except Exception as e:
+        return str(e)
 
-
+# Function to calculate the Relative Strength Index (RSI) for a given ticker
 def calculate_RSI(ticker):
-    data = yf.Ticker(ticker).history(period="1y").Close
-    delta = data.diff()
-    up = delta.clip(lower=0)
-    down = -1 * delta.clip(upper=0)
-    ema_up = up.ewm(com=14 - 1, adjust=False).mean()
-    ema_down = down.ewm(com=14 - 1, adjust=False).mean()
-    rs = ema_up / ema_down
-    rsi = 100 - (100 / (1 + rs)).iloc[-1]
-    return str(rsi)
+    try:
+        data = yf.Ticker(ticker).history(period="1y").Close
+        delta = data.diff()
+        up = delta.clip(lower=0)
+        down = -1 * delta.clip(upper=0)
+        ema_up = up.ewm(com=14 - 1, adjust=False).mean()
+        ema_down = down.ewm(com=14 - 1, adjust=False).mean()
+        rs = ema_up / ema_down
+        rsi = 100 - (100 / (1 + rs)).iloc[-1]
+        return str(rsi)
+    except Exception as e:
+        return str(e)
 
-
+# Function to calculate the Moving Average Convergence Divergence (MACD) for a given ticker
 def calculate_MACD(ticker):
-    data = yf.Ticker(ticker).history(period="1y").Close
-    short_EMA = data.ewm(span=12, adjust=False).mean()
-    long_EMA = data.ewm(span=26, adjust=False).mean()
-    MACD = short_EMA - long_EMA
-    signal = MACD.ewm(span=9, adjust=False).mean()
-    MACD_histogram = MACD - signal
-    return f"MACD: {MACD[-1]}, Signal Line: {signal[-1]}, MACD Histogram: {MACD_histogram[-1]}"
+    try:
+        data = yf.Ticker(ticker).history(period="1y").Close
+        short_EMA = data.ewm(span=12, adjust=False).mean()
+        long_EMA = data.ewm(span=26, adjust=False).mean()
+        MACD = short_EMA - long_EMA
+        signal = MACD.ewm(span=9, adjust=False).mean()
+        MACD_histogram = MACD - signal
+        return f"MACD: {MACD[-1]}, Signal Line: {signal[-1]}, MACD Histogram: {MACD_histogram[-1]}"
+    except Exception as e:
+        return str(e)
 
-
+# Function to plot stock price for a given ticker
 def plot_stock_price(ticker):
-    data = yf.Ticker(ticker).history(period="1y")
-    plt.figure(figsize=(10, 5))
-    plt.plot(data.index, data.Close)
-    plt.title(f"{ticker} Stock Price over the Last Year")
-    plt.xlabel("Date")
-    plt.ylabel("Stock Price ($)")
-    plt.grid(True)
-    plt.savefig("stock.png")
-    plt.close()
+    try:
+        data = yf.Ticker(ticker).history(period="1y")
+        plt.figure(figsize=(10, 5))
+        plt.plot(data.index, data.Close)
+        plt.title(f"{ticker} Stock Price over the Last Year")
+        plt.xlabel("Date")
+        plt.ylabel("Stock Price ($)")
+        plt.grid(True)
+        plt.savefig("stock.png")
+        plt.close()
+        return "Stock price plot generated."
+    except Exception as e:
+        return str(e)
 
 
 # Define function descriptions and parameters for OpenAI chatbot
